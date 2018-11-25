a91a52a30a2fc0112537d4099b1df7592abde4a3
Matt Tuma <matt@tuma.cc>
2018-11-25 11:16:00 -0600

    error handling

--- lifx.rb
+++ lifx.rb
@@ -9,13 +9,35 @@ class Lifx
 		payload['power'] = config['power'] unless config['power'].nil?
 		payload['fast'] = config['fast'] unless config['fast'].nil?
 		payload['color'] = config['color'] unless config['color'].nil?
-		RestClient::Request.execute(method: :put, url: "#{@lifx_url}#{id}/state", payload: payload, headers: {Authorization: @auth})
+		api_call('put', "#{id}/state", payload)
 	end
 
 	def get_all_light_ids
-		response = RestClient::Request.execute(method: :get, url: "#{@lifx_url}all" , headers: {Authorization: @auth})
+		response = api_call('get', 'all')
+		return if response.nil?	
 		array =	JSON.parse(response.body)
 		array.map! {|light| light['id']}
 		array.reject! {|id| id == 'd073d5324c3d'}
+	end
+
+	private
+
+	def rest_client 
+		 RestClient::Resource.new("#{@lifx_url}" , headers: {Authorization: @auth})
+	end
+
+	def api_call(action, path, payload = nil)
+		begin
+			if payload
+				response = rest_client[path].send(action, payload)
+			else
+				response = rest_client[path].send(action)
+			end
+		rescue RestClient::ExceptionWithResponse => err
+			puts("Found and handled exception in lifx.rb when calling API: #{err}")
+		rescue => err
+			abort("Unhandled exception found in lifx.rb when calling API: #{err}")
+		end
+		response
 	end
 end
--- test.rb
+++ test.rb
@@ -16,10 +16,16 @@ lifx_config['fast'] = 'true'
 sunrise = tz.sunrise
 sunset = tz.sunset
 
-puts sunrise
-puts sunset
+puts "Sunrise: #{sunrise}"
+puts "Sunset: #{sunset}"
+
+puts 'getting list of lifx lights'
 
 all_lights = lifx.get_all_light_ids
+
+abort('no lights found!') if all_lights.nil?
+
+puts "Found list of lights: #{all_lights.join(', ')}"
 
 while true do
 	if Time.now > sunrise && Time.now > sunset
@@ -32,18 +38,19 @@ while true do
                 puts sunset
 	end	
 	if Time.now < sunrise
-		puts 'it is nighttime'
+		puts 'it is nighttime, turning on all lights'
 		lifx_config['power'] = 'on'
 		all_lights.each  do |light_id|
 			lifx.modify_light(light_id, lifx_config)
                 end
 	else
-		puts 'it is daytime'
+		puts 'it is daytime, turning off all lights'
                 lifx_config['power'] = 'off'
                 all_lights.each  do |light_id|
                         lifx.modify_light(light_id, lifx_config)
                 end
 	end
+	puts('action completed')
 	sleep 60
 end
 
